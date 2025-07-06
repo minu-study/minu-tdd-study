d8690746fa8f710967691b9c1459fdb894dbb4cb
minu <min9288@gmail.com>
2025-07-06 22:13:50 +0900

    feat: 서비스 로직 분리

---
+++ src/main/java/io/hhplus/tdd/api/point/service/PointService.java
@@ -0,0 +1,51 @@
+package io.hhplus.tdd.api.point.service;
+
+import io.hhplus.tdd.common.vo.TransactionType;
+import io.hhplus.tdd.domain.point.PointHistoryTable;
+import io.hhplus.tdd.domain.user.UserPointTable;
+import io.hhplus.tdd.domain.point.model.PointHistory;
+import io.hhplus.tdd.domain.user.model.UserPoint;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    private final ConcurrentHashMap<Long, Lock> userLockMap = new ConcurrentHashMap<>();
+
+    private Lock getLock(long userId) {
+        return userLockMap.computeIfAbsent(userId, id -> new ReentrantLock());
+    }
+
+
+    public UserPoint getUserPoint(long id) {
+        return null;
+    }
+
+    public List<PointHistory> getPointHistory(long id) {
+        return null;
+    }
+
+    public UserPoint chargePoint(long id, long amount) {
+        return null;
+    }
+
+    public UserPoint usePoint(long id, long amount) {
+        return null;
+    }
+
+}
+
+
+
