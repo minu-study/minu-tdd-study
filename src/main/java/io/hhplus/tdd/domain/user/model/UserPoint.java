868249e43660a42797e27c52731e2875d4e1454f
minu <min9288@gmail.com>
2025-07-06 22:13:04 +0900

    refactor: 프로젝트 구조 변경

---
+++ src/main/java/io/hhplus/tdd/domain/user/model/UserPoint.java
@@ -0,0 +1,12 @@
+package io.hhplus.tdd.domain.user.model;
+
+public record UserPoint(
+        long id,
+        long point,
+        long updateMillis
+) {
+
+    public static UserPoint empty(long id) {
+        return new UserPoint(id, 0, System.currentTimeMillis());
+    }
+}
