12a40db28e0d72225ee43c7ece9bc37c77358a30
minu <min9288@gmail.com>
2025-07-07 22:32:25 +0900

    test: 포인트 충전

--- src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd;
 
 
 import io.hhplus.tdd.api.point.service.PointService;
+import io.hhplus.tdd.common.vo.TransactionType;
 import io.hhplus.tdd.domain.point.PointHistoryTable;
 import io.hhplus.tdd.domain.user.UserPointTable;
 import io.hhplus.tdd.domain.user.model.UserPoint;
@@ -13,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -47,6 +50,28 @@ public class PointServiceUnitTest {
         // then
         assertThat(userPoint.point()).isZero();
         verify(userPointTable).selectById(userId);
+    }
+
+    @Test
+    @DisplayName("포인트를 충전한다")
+    void chargePoint() {
+        // given
+        long userId = 1L;
+        long beforeAmount = 1000L;
+        long chargeAmount = 500L;
+        UserPoint beforePoint = new UserPoint(userId, beforeAmount, System.currentTimeMillis());
+        UserPoint afterPoint = new UserPoint(userId, beforeAmount + chargeAmount, System.currentTimeMillis());
+        given(userPointTable.selectById(userId)).willReturn(beforePoint);
+        given(userPointTable.insertOrUpdate(userId, beforeAmount + chargeAmount)).willReturn(afterPoint);
+
+        // when
+        UserPoint userPoint = pointService.chargePoint(userId, chargeAmount);
+
+        // then
+        assertThat(userPoint.point()).isEqualTo(beforeAmount + chargeAmount);
+        verify(userPointTable).selectById(userId);
+        verify(userPointTable).insertOrUpdate(userId, beforeAmount + chargeAmount);
+        verify(pointHistoryTable).insert(eq(userId), eq(chargeAmount), eq(TransactionType.CHARGE), anyLong());
     }
 
 
