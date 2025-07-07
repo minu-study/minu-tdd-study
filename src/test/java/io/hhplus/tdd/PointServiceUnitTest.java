172836a0cfb7974303cf070bc7180add1ac1e81b
minu <min9288@gmail.com>
2025-07-07 22:27:37 +0900

    test: 포인트 조회

--- src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
@@ -4,10 +4,17 @@ package io.hhplus.tdd;
 import io.hhplus.tdd.api.point.service.PointService;
 import io.hhplus.tdd.domain.point.PointHistoryTable;
 import io.hhplus.tdd.domain.user.UserPointTable;
+import io.hhplus.tdd.domain.user.model.UserPoint;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 public class PointServiceUnitTest {
 
@@ -25,7 +32,22 @@ public class PointServiceUnitTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    @Test
+    @DisplayName("사용자의 포인트를 조회한다")
+    void getUserPoint() {
 
+        // given
+        long userId = 1L;
+        UserPoint expectedPoint = new UserPoint(userId, 0L, System.currentTimeMillis());
+        given(userPointTable.selectById(userId)).willReturn(expectedPoint);
+
+        // when
+        UserPoint userPoint = pointService.getUserPoint(userId);
+
+        // then
+        assertThat(userPoint.point()).isZero();
+        verify(userPointTable).selectById(userId);
+    }
 
 
 
