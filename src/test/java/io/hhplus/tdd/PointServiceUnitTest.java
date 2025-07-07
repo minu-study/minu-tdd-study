dda1561a35eae250e4bc00456edd757dbc7620a4
minu <min9288@gmail.com>
2025-07-07 22:49:28 +0900

    refactor: 테스트 코드 public 접근제어 제거

--- src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceUnitTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
-public class PointServiceUnitTest {
+class PointServiceUnitTest {
 
     @Mock
     private UserPointTable userPointTable;
